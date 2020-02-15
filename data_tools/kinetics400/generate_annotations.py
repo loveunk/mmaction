10b7d62f0233fff292f8f91baed408d9457065d5
Kevin Qi <qiwen@qiwen.name>
2020-02-15 21:52:55 +0800

    Create generate_annotations.py

---
+++ data_tools/kinetics400/generate_annotations.py
@@ -0,0 +1,29 @@
+import os, glob, json, pwd
+import os.path as osp
+
+def main():
+
+    train_path = osp.join(os.getcwd(), '../../data/kinetics400/videos_train/')
+    val_path = osp.join(os.getcwd(), '../../data/kinetics400/videos_val/')
+    anno_path = osp.join(os.getcwd(), '../../data/kinetics400/annotations/')
+
+    with open(os.path.join(anno_path, "classids.json")) as f:
+        video_label2id=json.load(f)
+    video_label2id = {"_".join(x.strip('"').split(" ")):y for x,y in video_label2id.items()}
+
+    # generate train.csv
+    video_list = glob.glob(train_path+"*/*.mp4")
+    f = open(os.path.join(anno_path, "kinetics400_train_list_videos.txt"), "w")
+    for video in video_list:
+        f.write(video.split('.')[0] + " " + str(video_label2id[video.split('/')[-2]]) + "\n")
+    f.close()
+
+    # generate val.csv
+    video_list = glob.glob(val_path+"*/*.mp4")
+    f = open(os.path.join(anno_path, "kinetics400_val_list_videos.txt"), "w")
+    for video in video_list:
+        f.write(video.split('.')[0] + " " + str(video_label2id[video.split('/')[-2]]) + "\n")
+    f.close()
+
+if __name__ == "__main__":
+    main()
