3cc40e6c8e64d0a49eb79fa62a37e37fd72d60c1
Kevin Qi <qiwen@qiwen.name>
2020-02-16 11:13:30 +0800

    Update generate_annotations.py

--- data_tools/kinetics400/generate_annotations.py
+++ data_tools/kinetics400/generate_annotations.py
@@ -7,22 +7,26 @@ def main():
     val_path = osp.join(os.getcwd(), '../../data/kinetics400/videos_val/')
     anno_path = osp.join(os.getcwd(), '../../data/kinetics400/annotations/')
 
-    with open(os.path.join(anno_path, "classids.json")) as f:
+    with open(osp.join(anno_path, "classids.json")) as f:
         video_label2id=json.load(f)
     video_label2id = {"_".join(x.strip('"').split(" ")):y for x,y in video_label2id.items()}
 
     # generate train.csv
     video_list = glob.glob(train_path+"*/*.mp4")
-    f = open(os.path.join(anno_path, "kinetics400_train_list_videos.txt"), "w")
+    f = open(osp.join(anno_path, "../kinetics400_train_list_videos.txt"), "w")
     for video in video_list:
-        f.write(video.split('.')[0] + " " + str(video_label2id[video.split('/')[-2]]) + "\n")
+        dirname  = osp.dirname(video)
+        nameonly = osp.basename(video).split('.')[0]
+        f.write(osp.join(dirname, nameonly) + " " + str(video_label2id[video.split('/')[-2]]) + "\n")
     f.close()
 
     # generate val.csv
     video_list = glob.glob(val_path+"*/*.mp4")
-    f = open(os.path.join(anno_path, "kinetics400_val_list_videos.txt"), "w")
+    f = open(osp.join(anno_path, "../kinetics400_val_list_videos.txt"), "w")
     for video in video_list:
-        f.write(video.split('.')[0] + " " + str(video_label2id[video.split('/')[-2]]) + "\n")
+        dirname  = osp.dirname(video)
+        nameonly = osp.basename(video).split('.')[0]
+        f.write(osp.join(dirname, nameonly) + " " + str(video_label2id[video.split('/')[-2]]) + "\n")
     f.close()
 
 if __name__ == "__main__":
